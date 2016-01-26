4468c2cefb35899b1404ee61ea6acfd5828eb598
Firas Abuzaid <fabuzaid21@gmail.com>
2016-01-26 01:44:45 -0500

    python script to generate plots

---
+++ plots.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+
+DIRS = {
+        'by-feature': 1,
+        'by-instance': 0,
+        'by-tree-depth': 4,
+        'mnist-8m': 4,
+        'internet-web-company': 4
+        }
+FILES = ['byRow.tsv', 'byCol.tsv']
+
+
+plt.rc('text', usetex = True)
+
+for dir, val_index in DIRS.items():
+    plt.figure()
+    for file in FILES:
+        with open('tsvs/' + dir + '/' + file) as f:
+            header = f.readline()
+            x_label = header.split('\t')[val_index]
+            xs, ys = [], []
+            for line in f:
+                if line[0] == '#':
+                    continue
+                vals = line.split('\t')
+                x_val = int(vals[val_index])
+                y_val = float(vals[7])
+                # num_instances, num_features, train_data, test_data, depth, algo,\
+                # label, training_time, training_metric, test_metric\
+                # = int(vals[0]), int(vals[1]), vals[2], vals[3], int(vals[4]), vals[5],\
+                # vals[6], float(vals[7]), float(vals[8]), float(vals[9])
+                xs.append(x_val)
+                ys.append(y_val)
+            plt.plot(xs, ys, '.-', label = file[:-4])
+            plt.ylabel('Training Time (s)')
+            plt.xlabel(x_label)
+    plt.legend(loc='upper left')
+    plt.title(dir)
+    plt.grid(True)
+    plt.savefig(dir + '.eps')
+
