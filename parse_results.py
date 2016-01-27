83f0ff4132191500f1d48f0ac646adbc4f4d4494
Firas Abuzaid <fabuzaid21@gmail.com>
2016-01-27 15:20:37 -0500

    fixed bugs

--- parse_results.py
+++ parse_results.py
@@ -4,13 +4,13 @@ import re
 import sys
 import os
 
-PARAMS_REGEX = r'--num-examples=(\d+)\s--num-features=(\d+)\s--label-type=(\d+).*--tree-depth=(\d+).*--alg-type=(\w+).*--training-data=(\S*)\s--test-data=(\S*)\s'
+PARAMS_REGEX = r'--num-examples=(\d+)\s--num-features=(\d+)\s--label-type=(\d+).*--tree-depth=(\d+)\s--max-bins=(\d+)\s--alg-type=(\w+).*--training-data=(\S*)\s--test-data=(\S*)\s'
 TRAIN_TIME_REGEX = r'Training time: (\d+\.\d+)'
 TRAIN_METRIC_REGEX = r'Training Set Metric: (\d+\.\d+)'
 TEST_METRIC_REGEX = r'Test Set Metric: (\d+\.\d+)'
 
 DELIMITER='\t'
-HEADER_VALS = ['Rows', 'Columns', 'Train Data', 'Test Data', 'Depth', 'Algo', 'Label', 'Training Time (s)', 'Training Metric', 'Test Metric']
+HEADER_VALS = ['Rows', 'Columns', 'Train Data', 'Test Data', 'Depth', 'Max Bins', 'Algo', 'Label', 'Training Time (s)', 'Training Metric', 'Test Metric']
 TSV_HEADER = DELIMITER.join(HEADER_VALS) + '\n'
 TSV_DIR = 'tsvs/'
 
@@ -30,8 +30,8 @@ def main(argv):
                 m = re.search(PARAMS_REGEX, line)
                 num_examples, num_features,\
                 label_type, tree_depth,\
-                alg_type, train_data,\
-                test_data = m.group(1, 2, 3, 4, 5, 6, 7)
+                max_bins, alg_type,\
+                train_data, test_data = m.group(1, 2, 3, 4, 5, 6, 7, 8)
                 if (label_type == '0'):
                     label_type = 'Regression'
                 else:
@@ -51,18 +51,19 @@ def main(argv):
                 m = re.search(TEST_METRIC_REGEX, line)
                 test_set_metric = m.group(1)
 
-                if train_data is None:
-                    train_data = ''
+                if train_data == '':
+                    train_data = '   N/A   '
                 else:
                     train_data = re.sub(r'^hdfs.*9000\/', '', train_data)
-                if test_data is None:
-                    test_data = ''
+                if test_data == '':
+                    test_data = '   N/A   '
                 else:
                     test_data = re.sub(r'^hdfs.*9000\/', '', test_data)
 
                 result_line = [num_examples, num_features, train_data,\
-                               test_data, tree_depth, alg_type, label_type,\
-                               train_time, train_set_metric, test_set_metric]
+                               test_data, tree_depth, max_bins, alg_type,\
+                               label_type, train_time, train_set_metric,\
+                               test_set_metric]
 
                 tsv_file.write(DELIMITER.join(result_line) + '\n')
         tsv_file.close()
--- plots.py
+++ plots.py
@@ -3,30 +3,30 @@
 import matplotlib.pyplot as plt
 
 DIRS = {
-        'by-feature': 1,
-        'by-instance': 0,
-        'by-tree-depth': 4,
-        'mnist-8m': 4,
-        'internet-web-company': 4
+        'by-feature': (1, 8),
+        #'by-instance': 0,
+        #'by-tree-depth': 4,
+        'mnist-8m': (4, 7),
+        'internet-web-company': (4, 7)
         }
 FILES = ['byRow.tsv', 'byCol.tsv']
 
 
 plt.rc('text', usetex = True)
 
-for dir, val_index in DIRS.items():
+for dir, (x_val_index, y_val_index) in DIRS.items():
     plt.figure()
     for file in FILES:
         with open('tsvs/' + dir + '/' + file) as f:
             header = f.readline()
-            x_label = header.split('\t')[val_index]
+            x_label = header.split('\t')[x_val_index]
             xs, ys = [], []
             for line in f:
                 if line[0] == '#':
                     continue
                 vals = line.split('\t')
-                x_val = int(vals[val_index])
-                y_val = float(vals[7])
+                x_val = int(vals[x_val_index])
+                y_val = float(vals[y_val_index])
                 # num_instances, num_features, train_data, test_data, depth, algo,\
                 # label, training_time, training_metric, test_metric\
                 # = int(vals[0]), int(vals[1]), vals[2], vals[3], int(vals[4]), vals[5],\
